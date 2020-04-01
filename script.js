5e9cd20725e144259dab0281ee56537f10cb070b
wubuchako <56101474+wubuchako@users.noreply.github.com>
2020-04-01 22:11:55 +0900

    JS code added

--- script.js
+++ script.js
@@ -47,7 +47,45 @@ const search = document.getElementById('search'),
      }
   }
 
+  //Fetch meal by ID
+  function getMealById(mealID){
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0];
 
+      addMealToDOM(meal);
+    });
+  }
+
+  //Add meal to DOM
+  function addMealToDOM(meal){
+    const ingredients = [];
+
+    for(let i=1; i <= 20; i++){
+      if(meal[`strIngredient${i}`]){
+        ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+      } else {
+        break;
+      }
+    }
+  }
 
   //Event Listeners
    submit.addEventListener('submit', searchMeal);
+
+   mealsEl.addEventListener('click', e => {
+     const mealInfo = e.composedPath.find(item => {
+       if(item.classList){
+        return item.classList.contains('meal-info');
+       }else {
+         return false;
+       }
+     });
+
+     if(mealInfo){
+       const mealID = mealInfo.getAttribute('data-mealid');
+
+       getMealById(mealID);
+     }
+   });
